ade0a485f1fc95bcc4d23c648f9dfe9c1acfecfd
yanzhenxing <1753502691@qq.com>
2021-02-10 23:49:17 +0800

    Revert: Pandas 新内容2

--- Pandas/Bzhan/day09_图形的生产/demo/021/021.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-pd.options.display.max_columns = 999
-videos = pd.read_excel('C:/Temp/Videos.xlsx', index_col='Month')
-# table = videos.transpose()
-table = videos.T
-print(table)
